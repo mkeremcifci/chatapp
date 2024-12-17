233911ed6d507feed10ddd26bdfda974dda70a92
Mustafa Kerem Çifçi <cifcimustafakerem@gmail.com>
2024-12-17 23:10:33 +0300

    User authentication and token test

---
+++ request.py
@@ -0,0 +1,24 @@
+import requests
+
+BASE_URL = "http://127.0.0.1:8000"
+TOKEN_URL = f"{BASE_URL}/token"
+
+data = {
+    "username":"Kerem",
+    "password":"password"
+}
+
+def getToken():
+    response = requests.post(
+        TOKEN_URL,
+        data=data,
+    )
+    if response.status_code == 200:
+        token = response.json().get("access_token")
+        print(f"Token: {token}")
+        return token
+    else:
+        print(f"Token alınamadı {response.status_code} - {response.text}")
+        return None
+
+getToken()
